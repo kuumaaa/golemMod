61c9e3a1c22589ed018491d0275f37fc05fe5579
kuumaaa <73474197+kuumaaa@users.noreply.github.com>
2022-04-28 16:16:58 +0200

    Add fire trail for FireGolemEntity

--- src/main/java/com/kuuma/vanillagolems/entity/custom/FireGolemEntity.java
+++ src/main/java/com/kuuma/vanillagolems/entity/custom/FireGolemEntity.java
@@ -5,9 +5,13 @@ import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.tags.EntityTypeTags;
+import net.minecraft.tags.FluidTags;
 import net.minecraft.util.Mth;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
@@ -18,18 +22,27 @@ import net.minecraft.world.entity.ai.goal.RangedAttackGoal;
 import net.minecraft.world.entity.ai.goal.WaterAvoidingRandomStrollGoal;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.animal.SnowGolem;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.monster.Enemy;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.Fireball;
 import net.minecraft.world.entity.projectile.LargeFireball;
 import net.minecraft.world.entity.projectile.SmallFireball;
 import net.minecraft.world.entity.projectile.Snowball;
+import net.minecraft.world.item.ElytraItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
 
 public class FireGolemEntity extends SnowGolem {
+    private int noJumpDelay = 0;
+
     public FireGolemEntity(EntityType<? extends SnowGolem> p_29902_, Level p_29903_) {
         super(p_29902_, p_29903_);
     }
@@ -96,22 +109,151 @@ public class FireGolemEntity extends SnowGolem {
     }
 
     public void aiStep() {
-        super.aiStep();
+        if (this.noJumpDelay > 0) {
+            --this.noJumpDelay;
+        }
+
+        if (this.isControlledByLocalInstance()) {
+            this.lerpSteps = 0;
+            this.setPacketCoordinates(this.getX(), this.getY(), this.getZ());
+        }
+
+        if (this.lerpSteps > 0) {
+            double d0 = this.getX() + (this.lerpX - this.getX()) / (double)this.lerpSteps;
+            double d2 = this.getY() + (this.lerpY - this.getY()) / (double)this.lerpSteps;
+            double d4 = this.getZ() + (this.lerpZ - this.getZ()) / (double)this.lerpSteps;
+            double d6 = Mth.wrapDegrees(this.lerpYRot - (double)this.getYRot());
+            this.setYRot(this.getYRot() + (float)d6 / (float)this.lerpSteps);
+            this.setXRot(this.getXRot() + (float)(this.lerpXRot - (double)this.getXRot()) / (float)this.lerpSteps);
+            --this.lerpSteps;
+            this.setPos(d0, d2, d4);
+            this.setRot(this.getYRot(), this.getXRot());
+        } else if (!this.isEffectiveAi()) {
+            this.setDeltaMovement(this.getDeltaMovement().scale(0.98D));
+        }
+
+        if (this.lerpHeadSteps > 0) {
+            this.yHeadRot += (float)Mth.wrapDegrees(this.lyHeadRot - (double)this.yHeadRot) / (float)this.lerpHeadSteps;
+            --this.lerpHeadSteps;
+        }
+
+        Vec3 vec3 = this.getDeltaMovement();
+        double d1 = vec3.x;
+        double d3 = vec3.y;
+        double d5 = vec3.z;
+        if (Math.abs(vec3.x) < 0.003D) {
+            d1 = 0.0D;
+        }
+
+        if (Math.abs(vec3.y) < 0.003D) {
+            d3 = 0.0D;
+        }
+
+        if (Math.abs(vec3.z) < 0.003D) {
+            d5 = 0.0D;
+        }
+
+        this.setDeltaMovement(d1, d3, d5);
+        this.level.getProfiler().push("ai");
+        if (this.isImmobile()) {
+            this.jumping = false;
+            this.xxa = 0.0F;
+            this.zza = 0.0F;
+        } else if (this.isEffectiveAi()) {
+            this.level.getProfiler().push("newAi");
+            this.serverAiStep();
+            this.level.getProfiler().pop();
+        }
+
+        this.level.getProfiler().pop();
+        this.level.getProfiler().push("jump");
+        if (this.jumping && this.isAffectedByFluids()) {
+            double d7;
+            if (this.isInLava()) {
+                d7 = this.getFluidHeight(FluidTags.LAVA);
+            } else {
+                d7 = this.getFluidHeight(FluidTags.WATER);
+            }
+
+            boolean flag1 = this.isInWater() && d7 > 0.0D;
+            double d8 = this.getFluidJumpThreshold();
+            if (!flag1 || this.onGround && !(d7 > d8)) {
+                if (!this.isInLava() || this.onGround && !(d7 > d8)) {
+                    if ((this.onGround || flag1 && d7 <= d8) && this.noJumpDelay == 0) {
+                        this.jumpFromGround();
+                        this.noJumpDelay = 10;
+                    }
+                } else {
+                    this.jumpInLiquid(FluidTags.LAVA);
+                }
+            } else {
+                this.jumpInLiquid(FluidTags.WATER);
+            }
+        } else {
+            this.noJumpDelay = 0;
+        }
+
+        this.level.getProfiler().pop();
+        this.level.getProfiler().push("travel");
+        this.xxa *= 0.98F;
+        this.zza *= 0.98F;
+        this.updateFallFlying();
+        AABB aabb = this.getBoundingBox();
+        this.travel(new Vec3((double)this.xxa, (double)this.yya, (double)this.zza));
+        this.level.getProfiler().pop();
+        this.level.getProfiler().push("freezing");
+        boolean flag = this.getType().is(EntityTypeTags.FREEZE_HURTS_EXTRA_TYPES);
+        if (!this.level.isClientSide && !this.isDeadOrDying()) {
+            int i = this.getTicksFrozen();
+            if (this.isInPowderSnow && this.canFreeze()) {
+                this.setTicksFrozen(Math.min(this.getTicksRequiredToFreeze(), i + 1));
+            } else {
+                this.setTicksFrozen(Math.max(0, i - 2));
+            }
+        }
+
+        this.removeFrost();
+        this.tryAddFrost();
+        if (!this.level.isClientSide && this.tickCount % 40 == 0 && this.isFullyFrozen() && this.canFreeze()) {
+            int j = flag ? 5 : 1;
+            this.hurt(DamageSource.FREEZE, (float)j);
+        }
+
+        this.level.getProfiler().pop();
+        this.level.getProfiler().push("push");
+        if (this.autoSpinAttackTicks > 0) {
+            --this.autoSpinAttackTicks;
+            this.checkAutoSpinAttack(aabb, this.getBoundingBox());
+        }
+
+        this.pushEntities();
+        this.level.getProfiler().pop();
+        if (!this.level.isClientSide && this.isSensitiveToWater() && this.isInWaterRainOrBubble()) {
+            this.hurt(DamageSource.DROWN, 1.0F);
+        }
+
+        this.level.getProfiler().push("looting");
+        if (!this.level.isClientSide && this.canPickUpLoot() && this.isAlive() && !this.dead && net.minecraftforge.event.ForgeEventFactory.getMobGriefingEvent(this.level, this)) {
+            for(ItemEntity itementity : this.level.getEntitiesOfClass(ItemEntity.class, this.getBoundingBox().inflate(1.0D, 0.0D, 1.0D))) {
+                if (!itementity.isRemoved() && !itementity.getItem().isEmpty() && !itementity.hasPickUpDelay() && this.wantsToPickUp(itementity.getItem())) {
+                    this.pickUpItem(itementity);
+                }
+            }
+        }
+
+        this.level.getProfiler().pop();
         if (!this.level.isClientSide) {
             int i = Mth.floor(this.getX());
             int j = Mth.floor(this.getY());
             int k = Mth.floor(this.getZ());
             BlockPos blockpos = new BlockPos(i, j, k);
             Biome biome = this.level.getBiome(blockpos).value();
-            if (biome.shouldSnowGolemBurn(blockpos)) {
-                this.hurt(DamageSource.ON_FIRE, 1.0F);
-            }
 
             if (!net.minecraftforge.event.ForgeEventFactory.getMobGriefingEvent(this.level, this)) {
                 return;
             }
 
-            BlockState blockstate = Blocks.SNOW.defaultBlockState();
+            BlockState blockstate = Blocks.FIRE.defaultBlockState();
 
             for(int l = 0; l < 4; ++l) {
                 i = Mth.floor(this.getX() + (double)((float)(l % 2 * 2 - 1) * 0.25F));
@@ -122,6 +264,38 @@ public class FireGolemEntity extends SnowGolem {
                     this.level.setBlockAndUpdate(blockpos1, blockstate);
                 }
             }
+        }
+
+    }
+
+    private void updateFallFlying() {
+        boolean flag = this.getSharedFlag(7);
+        if (flag && !this.onGround && !this.isPassenger() && !this.hasEffect(MobEffects.LEVITATION)) {
+            ItemStack itemstack = this.getItemBySlot(EquipmentSlot.CHEST);
+            flag = itemstack.canElytraFly(this) && itemstack.elytraFlightTick(this, this.fallFlyTicks);
+            if (false) //Forge: Moved to ElytraItem
+                if (itemstack.is(Items.ELYTRA) && ElytraItem.isFlyEnabled(itemstack)) {
+                    flag = true;
+                    int i = this.fallFlyTicks + 1;
+                    if (!this.level.isClientSide && i % 10 == 0) {
+                        int j = i / 10;
+                        if (j % 2 == 0) {
+                            itemstack.hurtAndBreak(1, this, (p_147232_) -> {
+                                p_147232_.broadcastBreakEvent(EquipmentSlot.CHEST);
+                            });
+                        }
+
+                        this.gameEvent(GameEvent.ELYTRA_FREE_FALL);
+                    }
+                } else {
+                    flag = false;
+                }
+        } else {
+            flag = false;
+        }
+
+        if (!this.level.isClientSide) {
+            this.setSharedFlag(7, flag);
         }
 
     }
